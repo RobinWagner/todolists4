6bae6acb987a755c7bb1f319d12dbf5a0e755b19
Robin <robin-wagner@web.de>
2015-11-28 18:31:36 +0100

    Create session

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get "/login" => "sessions#new", as: "login"
+  delete "/logout" => "sessions#destroy", as: "logout"
+
   root to: 'todo_lists#index'
 
   resources :todo_lists do
